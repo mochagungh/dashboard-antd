da845c4ceb6199e1f48a4d0c092779bcf7147348
mochagungh <mochagungh@gmail.com>
2019-10-03 14:06:13 +0700

    navigasi dinamis

---
+++ src/config/router/index.js
@@ -0,0 +1,21 @@
+const pageRoutes = () => {
+  return [
+    {
+      link: '/',
+      icon: 'user',
+      name: 'Navigasi User'
+    },
+    {
+      link: '/camera',
+      icon: 'video-camera',
+      name: 'Navigasi Kamera'
+    },
+    {
+      link: '/upload',
+      icon: 'upload',
+      name: 'Navigasi Upload'
+    }
+  ];
+};
+
+export default pageRoutes();
--- src/container/layouts/Dashboard/Dashboard.js
+++ src/container/layouts/Dashboard/Dashboard.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import { Layout, Menu, Icon } from "antd";
-import { Route, Switch } from "react-router-dom";
-import "./Dashboard.css";
+import React, { useState } from 'react';
+import { Layout, Menu, Icon } from 'antd';
+import { Route, Switch, Link } from 'react-router-dom';
+import './Dashboard.css';
+import pageRoutes from '../../../config/router';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -19,73 +20,51 @@ const Dashboard = () => {
         collapsed={collapsed}
         onCollapse={onCollapsed}
         style={{
-          overflow: "auto",
-          height: "100vh",
-          position: "fixed",
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
           left: 0
         }}
       >
         <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-          <Menu.Item key="1">
-            <Icon type="user" />
-            <span className="nav-text">nav 1</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="video-camera" />
-            <span className="nav-text">nav 2</span>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="upload" />
-            <span className="nav-text">nav 3</span>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Icon type="bar-chart" />
-            <span className="nav-text">nav 4</span>
-          </Menu.Item>
-          <Menu.Item key="5">
-            <Icon type="cloud-o" />
-            <span className="nav-text">nav 5</span>
-          </Menu.Item>
-          <Menu.Item key="6">
-            <Icon type="appstore-o" />
-            <span className="nav-text">nav 6</span>
-          </Menu.Item>
-          <Menu.Item key="7">
-            <Icon type="team" />
-            <span className="nav-text">nav 7</span>
-          </Menu.Item>
-          <Menu.Item key="8">
-            <Icon type="shop" />
-            <span className="nav-text">nav 8</span>
-          </Menu.Item>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}>
+          {pageRoutes.map((data, i) => {
+            return (
+              <Menu.Item key={i}>
+                <Link to={data.link}>
+                  <Icon type={data.icon} />
+                  <span className="nav-text">{data.name}</span>
+                </Link>
+              </Menu.Item>
+            );
+          })}
         </Menu>
       </Sider>
       <Layout>
-        <Header style={{ background: "#fff", padding: 0 }}>
+        <Header style={{ background: '#fff', padding: 0 }}>
           <Icon
             className="trigger"
-            type={collapsed ? "menu-unfold" : "menu-fold"}
+            type={collapsed ? 'menu-unfold' : 'menu-fold'}
             onClick={onCollapsed}
           />
         </Header>
         <Content
           style={{
-            margin: "24px 16px",
+            margin: '24px 16px',
             padding: 24,
-            background: "#fff",
+            background: '#fff',
             minHeight: 280
           }}
         >
-          <div style={{ padding: 24, background: "#fff", textAlign: "center" }}>
+          <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
             <Switch>
               <Route exact path="/" component={() => <h1>ini ROOT</h1>} />
-              <Route path="/about" component={() => <h1>ini ABOUT</h1>} />
-              <Route path="/contact" component={() => <h1>ini CONTACT</h1>} />
+              <Route path="/camera" component={() => <h1>ini CAMERA</h1>} />
+              <Route path="/upload" component={() => <h1>ini UPLOAD</h1>} />
             </Switch>
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
+        <Footer style={{ textAlign: 'center' }}>
           Ant Design ©2018 Created by Ant UED
         </Footer>
       </Layout>
